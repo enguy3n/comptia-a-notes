a602af3b171b9857d695c46ee6f7dafbbd6cc728
enguy3n <ethan.nguyen931@gmail.com>
2026-04-08 01:08:06 -0600

    test deployment comparing href links and react useNavigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css'
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 import HeaderNav from './components/HeaderNav';
 import Notes from './components/Notes';
@@ -9,23 +9,26 @@ import Notes from './components/Notes';
 import Unit1 from './pages/Unit1'
 import Unit2 from './pages/Unit2';
 
-// function TestButton(){
-//   const navigate = useNavigate();
+function TestButton(){
+  const navigate = useNavigate();
 
-//   const handleClick = () => {
-//     navigate("/comptia-a-notes/unit1");
-//   };
+  const handleClick = () => {
+    navigate("/comptia-a-notes/unit1");
+  };
 
-//   return(
-//     <button onClick={handleClick}>this is a test component!</button>
-//   )
-// }
+  return(
+    <button onClick={handleClick}>this is a test component!</button>
+  )
+}
 
 function App() {
 
   return (
     <>
       <HeaderNav />
+
+      <TestButton />
+
       <Routes>
         <Route path = "/comptia-a-notes" element={<div className="text-content">This is a home page</div>} />
         <Route path = "/comptia-a-notes/unit1" element={<Notes unitNumber={1} title="test title" bodyContent={<Unit1 />}/>} />
