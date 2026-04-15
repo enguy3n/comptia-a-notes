f1443c309da66ec74883efe37bb141947412a4b6
enguy3n <ethan.nguyen931@gmail.com>
2026-04-14 22:10:38 -0600

    test deployment with navbar routers changed to react routers

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,4 +1,26 @@
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+type Props = {
+    location: string,
+    title: string,
+}
+
+function NavItem({location, title}: Props){
+    const navigate = useNavigate();
+    let destination:string = `/comptia-a-notes/${location}`
+
+    console.log("nav item function activated:"+destination);
+
+    const handleClick = () => {
+        navigate(destination);
+    };
+    return(
+        <>
+            <NavDropdown.Item onClick={handleClick}>{title}</NavDropdown.Item>
+        </>
+    )
+}
 
 function HeaderNav(){
     return(
@@ -6,8 +28,8 @@ function HeaderNav(){
             <Nav className="me-auto">
                 <Nav.Link href="/comptia-a-notes/">Home</Nav.Link>
                 <NavDropdown title="Notes" id="notes-dropdown">
-                    <NavDropdown.Item href="/comptia-a-notes/unit1/">Unit 1</NavDropdown.Item>
-                    <NavDropdown.Item href="/comptia-a-notes/unit2/">Unit 2</NavDropdown.Item>
+                    <NavItem location="unit1" title="Unit 1"/>
+                    <NavItem location="unit2" title="unit2" />
                 </NavDropdown>
             </Nav>
         </Navbar>
